61c45c16d33bb2cd92436477161654647a4ac637
Jose Sitanggang <josestnggng@gmail.com>
2019-04-29 13:30:52 +0700

    init project

--- app/app.go
+++ app/app.go
@@ -1,1 +1,32 @@
 package app
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	"log"
+)
+
+type App struct{
+	Router *mux.Router	
+	Logger *log.Logger
+	Db *gorm.DB
+}
+
+func GetDB(){
+
+}
+
+func New(){
+
+}
+
+func (A *App) Run(port string){
+
+}
+
+// MIDDLEWARE
+
+
+// WRAPPER METHOD
+
---
+++ app/config.go
@@ -0,0 +1,8 @@
+package app
+
+type Config struct {
+	DbUser    string
+	DbPass    string
+	DbName    string
+	DbDialect string
+}
