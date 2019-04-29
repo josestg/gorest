10dd0b9c21eca5810510bc3f77ce6972deb27f09
Jose Sitanggang <jose.stnggng@gmail.com>
2019-04-29 14:09:52 +0700

    implementasi-feature-get-category

--- app/category.go
+++ app/category.go
@@ -37,7 +37,19 @@ func (A *App) CreateCategory(w http.ResponseWriter, r *http.Request){
 
 // GetCategory : GET /api/categories/id
 func (A *App) GetCategory(w http.ResponseWriter, r *http.Request){
+	var res Category
+	var params = getVars(r)
+	var id,err = parserID(params["id"])
+	if err!=nil{
+		A.RespondError(w, http.StatusBadRequest,err.Error())
+		return
+	}
+	if err:= A.Db.First(&res, Category{ID: id}).Error; err!=nil{
+		A.RespondError(w,http.StatusInternalServerError,err.Error())
+		return
+	}
 
+	A.RespondJSON(w,http.StatusOK,res)
 }
 
 // UpdateCategory : PUT /api/categories/id
