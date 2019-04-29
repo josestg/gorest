447d4185ad56ecca03f544ce13df2d63e26dc8d0
Jose Sitanggang <josestnggng@gmail.com>
2019-04-29 13:41:45 +0700

    core app

--- app/app.go
+++ app/app.go
@@ -1,10 +1,14 @@
 package app
 
 import (
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
 type App struct{
@@ -13,20 +17,75 @@ type App struct{
 	Db *gorm.DB
 }
 
-func GetDB(){
-
+func GetDB(uri, dialect string) *gorm.DB{
+	var db, err = gorm.Open(dialect,uri)
+	if err !=nil{
+		panic(fmt.Errorf("Error while connectin to DB %s\n",
+			err.Error()))
+		return nil
+	}
+	db.AutoMigrate(&Product{},&Image{},&Category{},
+		&CategoryProduct{},&ProductImage{})
+	return db
 }
 
-func New(){
+func New(c Config) *App{
 
+	logger := log.New(os.Stdout,"Log : ",log.LstdFlags| log.Lshortfile)
+	uri := fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True",
+		c.DbUser,c.DbPass,c.DbName)
+
+	var app App
+	app.Router = mux.NewRouter()
+	app.Logger = logger
+	app.Db = GetDB(uri,c.DbDialect)
+	return &app
 }
 
 func (A *App) Run(port string){
+	A.Logger.Printf("App start on port \t\t%s\n",port)
+	if err  := http.ListenAndServe(port, A.Router);
+		err !=nil {
+		panic("Port not available!")
+	}
+}
 
+// Utility
+func getVars(r *http.Request) map[string]string {
+	return mux.Vars(r)
 }
 
 // MIDDLEWARE
-
+// Log
+func (A *App) Log(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		startTime :=time.Now()
+		defer A.Logger.Printf("%s %s\t : Request processed in %s\n",
+			r.Method, r.URL.Path,time.Now().Sub(startTime),
+		)
+		next(w,r)
+	}
+}
 
 // WRAPPER METHOD
+func (A *App) Get(route string, fn func(w http.ResponseWriter, r *http.Request)){
+	A.Router.HandleFunc(route,
+		A.Log(fn)).Methods("GET")
+}
+
+func (A *App) Post(route string, fn func(w http.ResponseWriter, r *http.Request)){
+	A.Router.HandleFunc(route,
+		A.Log(fn)).Methods("POST")
+}
+
+func (A *App) Put(route string, fn func(w http.ResponseWriter, r *http.Request)){
+	A.Router.HandleFunc(route,
+		A.Log( fn)).Methods("PUT")
+}
+
+func (A *App) Del(route string, fn func(w http.ResponseWriter, r *http.Request)){
+	A.Router.HandleFunc(route,
+		A.Log(fn)).Methods("DELETE")
+}
+
 
--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import "myprestest/app"
+
 func main() {
+	app := app.New(app.Config{
+		DbUser: "root",
+		DbName:    "pretest",
+		DbDialect: "mysql",
+	})
+
+	app.Run(":8000")
 
 }
