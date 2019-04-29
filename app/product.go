47a44c70be26bf5da330a9a913989b8a3f141918
Jose Sitanggang <josestnggng@gmail.com>
2019-04-29 13:55:40 +0700

    implementasi-feature-update-Product

--- app/product.go
+++ app/product.go
@@ -53,7 +53,32 @@ func (A *App) GetProduct(w http.ResponseWriter, r *http.Request){
 
 // UpdateProduct : PUT /api/products/id
 func (A *App) UpdateProduct(w http.ResponseWriter, r *http.Request){
+	var res Product
+	var decoder = json.NewDecoder(r.Body)
+	var params = getVars(r)
+	var id,err = parserID(params["id"])
+	if err != nil{
+		A.RespondError(w, http.StatusBadRequest,err.Error())
+		return
+	}
+	if err:= A.Db.Find(&res, Product{ID: id}).Error; err!=nil{return}
+	if err:= decoder.Decode(&res); err!=nil{
+		A.RespondError(w,http.StatusBadRequest,err.Error())
+		return
+	}
+	defer r.Body.Close()
 
+	// keep ID
+	res.ID = id
+	if err:=A.Db.Save(&res).Error; err!=nil{
+		A.RespondError(w, http.StatusInternalServerError,err.Error())
+		return
+	}
+
+	A.RespondJSON(w, http.StatusOK,&Response{
+		Success:true,
+		Data:res,
+	})
 }
 
 // DeleteProduct : Delete /api/products/id
