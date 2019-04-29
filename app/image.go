bd4a4a0d5d9d53dc0a07590a285441671933709c
Jose Sitanggang <jose.stnggng@gmail.com>
2019-04-29 14:15:56 +0700

    implementasi-feature-get-images

--- app/image.go
+++ app/image.go
@@ -10,8 +10,12 @@ func UploadImage(filename *string,r *http.Request)  {
 
 // GetImages : GET /api/images
 func (A *App) GetImages(w http.ResponseWriter, r *http.Request){
-
-
+	var res []Image
+	if err :=A.Db.Find(&res).Error; err !=nil {
+		A.RespondError(w,http.StatusBadRequest,err.Error())
+		return
+	}
+	A.RespondJSON(w,http.StatusOK,res)
 }
 
 // CreateImage : POST /api/images
