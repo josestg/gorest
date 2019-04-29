105452fab95bd346143b49e7100ee33b05429664
Jose Sitanggang <jose.stnggng@gmail.com>
2019-04-29 14:40:11 +0700

    implementasi-feature-update-product-image

--- app/product_images.go
+++ app/product_images.go
@@ -95,7 +95,37 @@ func (A *App) GetProductImage(w http.ResponseWriter, r *http.Request){
 
 // UpdateProduct : PUT /api/product-images/pid/iid
 func (A *App) UpdateProductImage(w http.ResponseWriter, r *http.Request){
+	var res ProductImage
+	var decoder = json.NewDecoder(r.Body)
+	var params = getVars(r)
+	pid,err := parserID(params["pid"])
+	if err != nil{
+		A.RespondError(w, http.StatusBadRequest,err.Error())
+		return
+	}
 
+	iid,err := parserID(params["iid"])
+	if err != nil{
+		A.RespondError(w, http.StatusBadRequest,err.Error())
+		return
+	}
+
+	if err:= A.Db.Delete(&res, ProductImage{ProductID: pid,ImageID:iid}).Error; err!=nil{return}
+	if err:= decoder.Decode(&res); err!=nil{
+		A.RespondError(w,http.StatusBadRequest,err.Error())
+		return
+	}
+	defer r.Body.Close()
+
+	if err:=A.Db.Save(&res).Error; err!=nil{
+		A.RespondError(w, http.StatusInternalServerError,err.Error())
+		return
+	}
+
+	A.RespondJSON(w, http.StatusOK,&Response{
+		Success:true,
+		Data:res,
+	})
 }
 
 // DeleteProduct : Delete /api/product-images/pid/iid
