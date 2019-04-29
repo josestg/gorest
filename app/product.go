e82902296807e786c38434190c30b3e2b553bc86
Jose Sitanggang <josestnggng@gmail.com>
2019-04-29 13:45:06 +0700

    membuat abstrak router

---
+++ app/product.go
@@ -0,0 +1,29 @@
+package app
+
+import (
+	"net/http"
+)
+// GetProducts : GET /api/products
+func (A *App) GetProducts(w http.ResponseWriter, r *http.Request){
+
+}
+
+// CreateProduct : POST /api/products
+func (A *App) CreateProduct(w http.ResponseWriter, r *http.Request){
+
+}
+
+// GetProduct : GET /api/products/id
+func (A *App) GetProduct(w http.ResponseWriter, r *http.Request){
+
+}
+
+// UpdateProduct : PUT /api/products/id
+func (A *App) UpdateProduct(w http.ResponseWriter, r *http.Request){
+
+}
+
+// DeleteProduct : Delete /api/products/id
+func (A *App) DeleteProduct(w http.ResponseWriter, r *http.Request){
+
+}
--- app/router.go
+++ app/router.go
@@ -1,1 +1,12 @@
 package app
+
+func (A *App) SetupRouter(){
+
+	// Product Endpoints
+	A.Get("/api/products",A.GetProducts)
+	A.Post("/api/products",A.CreateProduct)
+	A.Get("/api/products/{id}",A.GetProduct)
+	A.Put("/api/products/{id}",A.UpdateProduct)
+	A.Del("/api/products/{id}",A.DeleteProduct)
+
+}
