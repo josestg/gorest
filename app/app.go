0baf33fdad7ff20db0c9450dcac2111060371061
Jose Sitanggang <jose.stnggng@gmail.com>
2019-05-02 13:46:13 +0700

    Update app.go

--- app/app.go
+++ app/app.go
@@ -25,11 +25,14 @@ func GetDB(uri, dialect string) *gorm.DB{
 		return nil
 	}
 	db.AutoMigrate(&Product{},&Image{},&Category{},&CategoryProduct{},&ProductImage{})
+	//hapus kode berikut jika memperbolehkan pengapusahan image dan category dari table
+	// category_product dan product_images
 	db.Model(&CategoryProduct{}).
 		AddForeignKey("product_id","products(id)","CASCADE","CASCADE")
 	db.Model(&CategoryProduct{}).
 		AddForeignKey("category_id","categories(id)","CASCADE","CASCADE")
 	db.Exec("ALTER TABLE `category_products` ADD UNIQUE(product_id,category_id)")
+	// sampai disini
 
 	db.Model(&ProductImage{}).AddForeignKey("product_id","products(id)","CASCADE","CASCADE")
 	db.Model(&ProductImage{}).AddForeignKey("image_id","images(id)","CASCADE","CASCADE")
